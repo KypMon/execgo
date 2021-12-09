c26cfa843e2a0a9b3ea0ad083487bf34a1a50603
KypMon <puyu.yang@cn.ey.com>
2021-12-09 12:44:38 +0800

    feat: chap10

---
+++ chap10/src/go.mod
@@ -0,0 +1,3 @@
+module goexercise/chap03/01
+
+go 1.17
---
+++ chap10/src/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var levelMap = make(map[int]string)
+
+func main() {
+	// initialize log level
+	levelMap[0] = "[SYSTEM] "
+	levelMap[1] = "[INFO] "
+	levelMap[2] = "[WARNING] "
+	levelMap[3] = "[ERROR] "
+
+	// HANDLER
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/healthz", healthz)
+
+	// provide an default env variable
+	_, isLogLevelPresent := os.LookupEnv("LOG_LEVEL")
+	if !isLogLevelPresent {
+		os.Setenv("LOG_LEVEL", "0")
+	}
+
+	_, isPortPresent := os.LookupEnv("PORT")
+	if !isPortPresent {
+		os.Setenv("PORT", "8080")
+	}
+
+	_, isVersionPresent := os.LookupEnv("VERSION")
+	if !isVersionPresent {
+		os.Setenv("VERSION", "1.4.0")
+	}
+
+	//fmt.Println("Listening " + os.Getenv("PORT") + ": ", 2)
+
+	logger("Listening "+os.Getenv("PORT")+": ", 1)
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+func logger(msg string, logLevel int) {
+
+	lvl, _ := strconv.Atoi(os.Getenv("LOG_LEVEL"))
+
+	// if this message log level is higher/more verbose than the preset level, then output
+	if logLevel >= lvl {
+		fmt.Println(levelMap[logLevel] + msg)
+	}
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	// #1: add delay here
+	delay := randInt(0, 2000)
+	fmt.Printf("Sleeping %d milliseconds...\n", delay)
+	time.Sleep(time.Millisecond * time.Duration(delay))
+
+	var statusCode = http.StatusOK
+	setHeader(w, r, statusCode)
+	w.WriteHeader(statusCode)
+
+	io.WriteString(w, "welcome to home page!")
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	// 4. return 200 for healthz
+	var statusCode = http.StatusOK
+
+	setHeader(w, r, statusCode)
+
+	w.WriteHeader(statusCode)
+
+	io.WriteString(w, "welcome to healthz!")
+}
+
+func setHeader(w http.ResponseWriter, r *http.Request, statusCode int) {
+	// 1. set req header to resp header
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			w.Header().Set(name, h)
+		}
+	}
+
+	// 2. set version as the header
+	w.Header().Set("VERSION", os.Getenv("VERSION"))
+
+	ip, err := getIP(r)
+	//fmt.Println(ip)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("No valid ip"))
+	}
+
+	// 3 record ip and status code
+	logger("the IP of this request is: "+ip+"The Status of the request is: "+strconv.Itoa(statusCode), 1)
+
+	//io.WriteString(w, "welcome to healthz!")
+}
+
+func getIP(r *http.Request) (string, error) {
+	//Get IP from the X-REAL-IP header
+	ip := r.Header.Get("X-REAL-IP")
+	netIP := net.ParseIP(ip)
+	if netIP != nil {
+		return ip, nil
+	}
+
+	//Get IP from X-FORWARDED-FOR header
+	ips := r.Header.Get("X-FORWARDED-FOR")
+	splitIps := strings.Split(ips, ",")
+	for _, ip := range splitIps {
+		netIP := net.ParseIP(ip)
+		if netIP != nil {
+			return ip, nil
+		}
+	}
+
+	//Get IP from RemoteAddr
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+	netIP = net.ParseIP(ip)
+	if netIP != nil {
+		return ip, nil
+	}
+	return "", fmt.Errorf("No valid ip found")
+}
