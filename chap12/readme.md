810290434fed115e72e492928e9a6058f5ec8084
Puyu Yang <978634806@qq.com>
2021-12-20 10:29:18 +0000

    feat: update tls and src v2 for canary release

--- chap12/readme.md
+++ chap12/readme.md
@@ -46,7 +46,7 @@ king get svc
 
 ## Apply the istio specs
 ```
-k apply -f ./istiospec/
+k apply -f ./istiospec/ingress.yaml
 ```
 
 try to get the page from MacBook
@@ -83,4 +83,80 @@ curl -v -H "Host: cncamp.com" 192.168.34.2:32582
 <
 * Connection #0 to host 192.168.34.2 left intact
 welcome to home page!* Closing connection 0
+```
+
+## Generate the Cert
+```shell
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=cncamp Inc./CN=*.cncamp.com' -keyout cncamp.com.key -out cncamp.com.crt
+
+kubectl create -n istio-ingress secret tls cncamp-istio-credential --key=cncamp.com.key --cert=cncamp.com.crt
+```
+
+## apply the new ssl enabled ingress spec
+```
+k apply -f ./istiospec/ingress-with-cert.yml
+
+curl --resolve cncamp.com:443:10.97.74.3 https://cncamp.com/healthz -v -k
+```
+
+```
+* Added cncamp.com:443:10.97.74.3 to DNS cache
+* Hostname cncamp.com was found in DNS cache
+*   Trying 10.97.74.3:443...
+* TCP_NODELAY set
+* Connected to cncamp.com (10.97.74.3) port 443 (#0)
+* ALPN, offering h2
+* ALPN, offering http/1.1
+* successfully set certificate verify locations:
+*   CAfile: /etc/ssl/certs/ca-certificates.crt
+  CApath: /etc/ssl/certs
+* TLSv1.3 (OUT), TLS handshake, Client hello (1):
+* TLSv1.3 (IN), TLS handshake, Server hello (2):
+* TLSv1.3 (IN), TLS handshake, Encrypted Extensions (8):
+* TLSv1.3 (IN), TLS handshake, Certificate (11):
+* TLSv1.3 (IN), TLS handshake, CERT verify (15):
+* TLSv1.3 (IN), TLS handshake, Finished (20):
+* TLSv1.3 (OUT), TLS change cipher, Change cipher spec (1):
+* TLSv1.3 (OUT), TLS handshake, Finished (20):
+* SSL connection using TLSv1.3 / TLS_AES_256_GCM_SHA384
+* ALPN, server accepted to use h2
+* Server certificate:
+*  subject: O=cncamp Inc.; CN=*.cncamp.com
+*  start date: Dec 20 08:48:24 2021 GMT
+*  expire date: Dec 20 08:48:24 2022 GMT
+*  issuer: O=cncamp Inc.; CN=*.cncamp.com
+*  SSL certificate verify result: self signed certificate (18), continuing anyway.
+* Using HTTP2, server supports multi-use
+* Connection state changed (HTTP/2 confirmed)
+* Copying HTTP/2 data in stream buffer to connection buffer after upgrade: len=0
+* Using Stream ID: 1 (easy handle 0x555b52d6de10)
+> GET /healthz HTTP/2
+> Host: cncamp.com
+> user-agent: curl/7.68.0
+> accept: */*
+>
+* TLSv1.3 (IN), TLS handshake, Newsession Ticket (4):
+* TLSv1.3 (IN), TLS handshake, Newsession Ticket (4):
+* old SSL session ID is stale, removing
+* Connection state changed (MAX_CONCURRENT_STREAMS == 2147483647)!
+< HTTP/2 200
+< accept: */*
+< user-agent: curl/7.68.0
+< version: 1.3.0
+< x-b3-sampled: 0
+< x-b3-spanid: b3ad3e72c40ca1b2
+< x-b3-traceid: 6fb0254853bf4cf8b3ad3e72c40ca1b2
+< x-envoy-attempt-count: 1
+< x-envoy-internal: true
+< x-forwarded-for: 10.0.2.15
+< x-forwarded-proto: https
+< x-request-id: e3ce2fa3-372f-4dd1-a97b-da58bf0534d5
+< date: Mon, 20 Dec 2021 09:24:46 GMT
+< content-length: 19
+< content-type: text/plain; charset=utf-8
+< x-envoy-upstream-service-time: 5
+< server: istio-envoy
+<
+* Connection #0 to host cncamp.com left intact
+welcome to healthz!
 ```
---
+++ chap12/srcV2/main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"io"
+	"math/rand"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var levelMap = make(map[int]string)
+
+func main() {
+	// initialize log level
+	levelMap[0] = "[SYSTEM] "
+	levelMap[1] = "[INFO] "
+	levelMap[2] = "[WARNING] "
+	levelMap[3] = "[ERROR] "
+
+	// register metrics
+	Register()
+
+	// HANDLER
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/healthz", healthz)
+	http.Handle("/metrics", promhttp.Handler())
+
+	// provide an default env variable
+	_, isLogLevelPresent := os.LookupEnv("LOG_LEVEL")
+	if !isLogLevelPresent {
+		os.Setenv("LOG_LEVEL", "0")
+	}
+
+	_, isPortPresent := os.LookupEnv("PORT")
+	if !isPortPresent {
+		os.Setenv("PORT", "8080")
+	}
+
+	_, isVersionPresent := os.LookupEnv("VERSION")
+	if !isVersionPresent {
+		os.Setenv("VERSION", "1.4.0")
+	}
+
+	//fmt.Println("Listening " + os.Getenv("PORT") + ": ", 2)
+
+	logger("Listening "+os.Getenv("PORT")+": ", 1)
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+func logger(msg string, logLevel int) {
+
+	lvl, _ := strconv.Atoi(os.Getenv("LOG_LEVEL"))
+
+	// if this message log level is higher/more verbose than the preset level, then output
+	if logLevel >= lvl {
+		fmt.Println(levelMap[logLevel] + msg)
+	}
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	// register delay metrics
+	timer := NewTimer()
+	defer timer.ObserveTotal()
+	
+	// add delay here
+	delay := randInt(0, 2000)
+	fmt.Printf("Sleeping %d milliseconds...\n", delay)
+	time.Sleep(time.Millisecond * time.Duration(delay))
+
+	var statusCode = http.StatusOK
+	setHeader(w, r, statusCode)
+	w.WriteHeader(statusCode)
+
+	io.WriteString(w, "welcome to home page V2!")
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	// 4. return 200 for healthz
+	var statusCode = http.StatusOK
+
+	setHeader(w, r, statusCode)
+
+	w.WriteHeader(statusCode)
+
+	io.WriteString(w, "welcome to healthz V2!")
+}
+
+func setHeader(w http.ResponseWriter, r *http.Request, statusCode int) {
+	// 1. set req header to resp header
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			w.Header().Set(name, h)
+		}
+	}
+
+	// 2. set version as the header
+	w.Header().Set("VERSION", os.Getenv("VERSION"))
+
+	ip, err := getIP(r)
+	//fmt.Println(ip)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("No valid ip"))
+	}
+
+	// 3 record ip and status code
+	logger("the IP of this request is: "+ip+"The Status of the request is: "+strconv.Itoa(statusCode), 1)
+
+	//io.WriteString(w, "welcome to healthz!")
+}
+
+func getIP(r *http.Request) (string, error) {
+	//Get IP from the X-REAL-IP header
+	ip := r.Header.Get("X-REAL-IP")
+	netIP := net.ParseIP(ip)
+	if netIP != nil {
+		return ip, nil
+	}
+
+	//Get IP from X-FORWARDED-FOR header
+	ips := r.Header.Get("X-FORWARDED-FOR")
+	splitIps := strings.Split(ips, ",")
+	for _, ip := range splitIps {
+		netIP := net.ParseIP(ip)
+		if netIP != nil {
+			return ip, nil
+		}
+	}
+
+	//Get IP from RemoteAddr
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+	netIP = net.ParseIP(ip)
+	if netIP != nil {
+		return ip, nil
+	}
+	return "", fmt.Errorf("No valid ip found")
+}
+
+func Register() {
+	err := prometheus.Register(functionLatency)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+const (
+	MetricsNamespace = "httpserver"
+)
+
+// NewExecutionTimer provides a timer for Updater's RunOnce execution
+func NewTimer() *ExecutionTimer {
+	return NewExecutionTimer(functionLatency)
+}
+
+var (
+	functionLatency = CreateExecutionTimeMetric(MetricsNamespace,
+		"Time spent.")
+)
+
+// NewExecutionTimer provides a timer for admission latency; call ObserveXXX() on it to measure
+func NewExecutionTimer(histo *prometheus.HistogramVec) *ExecutionTimer {
+	now := time.Now()
+	return &ExecutionTimer{
+		histo: histo,
+		start: now,
+		last:  now,
+	}
+}
+
+// ObserveTotal measures the execution time from the creation of the ExecutionTimer
+func (t *ExecutionTimer) ObserveTotal() {
+	(*t.histo).WithLabelValues("total").Observe(time.Now().Sub(t.start).Seconds())
+}
+
+// CreateExecutionTimeMetric prepares a new histogram labeled with execution step
+func CreateExecutionTimeMetric(namespace string, help string) *prometheus.HistogramVec {
+	return prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: namespace,
+			Name:      "execution_latency_seconds",
+			Help:      help,
+			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 15),
+		}, []string{"step"},
+	)
+}
+
+// ExecutionTimer measures execution time of a computation, split into major steps
+// usual usage pattern is: timer := NewExecutionTimer(...) ; compute ; timer.ObserveStep() ; ... ; timer.ObserveTotal()
+type ExecutionTimer struct {
+	histo *prometheus.HistogramVec
+	start time.Time
+	last  time.Time
+}
